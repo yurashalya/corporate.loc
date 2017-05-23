9fcb366866f1a0597eba32ad9bc99173f1c7958a
yurashalya <stallker9696@gmail.com>
2017-05-23 14:49:53 +0300

    Создание раздела МЕНЮ

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -63,9 +63,10 @@ Route::group(['prefix' => 'admin','middleware'=> 'auth'],function() {
 
     Route::resource('/permissions','Admin\PermissionsController');
 
+    Route::resource('/menus','Admin\MenusController');
+
 });
 
 
 
-																						
-	
+																																											
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -44,6 +44,10 @@ class AuthServiceProvider extends ServiceProvider
             return $user->canDo('EDIT_USERS', FALSE);
         });
 
+        $gate->define('VIEW_ADMIN_MENU', function ($user) {
+            return $user->canDo('VIEW_ADMIN_MENU', FALSE);
+        });
+
         //
     }
 }
