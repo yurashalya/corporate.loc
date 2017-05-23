6e2b1a234b3e21b480ce102aebc175a34f6b6971
yurashalya <stallker9696@gmail.com>
2017-05-23 09:12:24 +0300

    Создание и главной страницы админ панели

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,10 +44,24 @@ Route::resource('comment','CommentController',['only'=>['store']]);
 
 Route::match(['get','post'],'/contacts',['uses'=>'ContactsController@index','as'=>'contacts']);
 
+//php artisan make:auth
 Route::get('login','Auth\AuthController@showLoginForm');
 
 Route::post('login','Auth\AuthController@login');
 
 Route::get('logout','Auth\AuthController@logout');
 
-																						
+
+//admin
+Route::group(['prefix' => 'admin','middleware'=> 'auth'],function() {
+
+    //admin
+    Route::get('/',['uses' => 'Admin\IndexController@index','as' => 'adminIndex']);
+
+    Route::resource('/articles','Admin\ArticlesController');
+
+});
+
+
+
+	
