013d625cc524fa508e74e1089385a32887879d02
yurashalya <stallker9696@gmail.com>
2017-05-23 05:20:35 +0300

    Настройка отображения правого сайд бара в разделе блог

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -6,18 +6,20 @@ use Illuminate\Http\Request;
 
 use Corp\Repositories\PortfoliosRepository;
 use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\CommentsRepository;
 
 use Corp\Http\Requests;
 
 class ArticlesController extends SiteController
 {
 
-    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep) {
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep, CommentsRepository $c_rep) {
 
         parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
 
         $this->p_rep = $p_rep;
         $this->a_rep = $a_rep;
+        $this->c_rep = $c_rep;
 
         $this->bar = 'right';
 
@@ -34,8 +36,25 @@ class ArticlesController extends SiteController
         $content = view(env('THEME').'.articles_content')->with('articles',$articles)->render();
         $this->vars = array_add($this->vars,'content',$content);
 
+        $comments = $this->getComments(config('settings.recent_comments'));
+        $portfolios = $this->getPortfolios(config('settings.recent_portfolios'));
+
+
+        $this->contentRightBar = view(env('THEME').'.articlesBar')->with(['comments' => $comments,'portfolios' => $portfolios]);
+
 
         return $this->renderOutput();
+    }
+ 
+    public function getComments($take) {
+
+        $comments = $this->c_rep->get(['text','name','email','site','article_id','user_id'],$take);
+        return $comments;
+    }
+
+    public function getPortfolios($take) {
+        $portfolios = $this->p_rep->get(['title','text','alias','customer','img','filter_alias'],$take);
+        return $portfolios;
     }
 
     public function getArticles($alias = FALSE) {
